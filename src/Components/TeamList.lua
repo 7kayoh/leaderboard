1fae008ccfa139f7282e0f8009b143ac1edd34dd
7kayoh <git@7kayoh.net>
2022-04-08 00:22:44 +0900

    Reduced states

---
+++ src/Components/TeamList.lua
@@ -0,0 +1,65 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Fusion = require(ReplicatedStorage.Packages.Fusion)
+local PlayerComponent = require(script.Parent.Player)
+local TeamHeaderComponent = require(script.Parent.TeamHeader)
+local Player = Players.LocalPlayer
+
+local New = Fusion.New
+local Children = Fusion.Children
+local Value = Fusion.Value
+local ForPairs = Fusion.ForPairs
+local Computed = Fusion.Computed
+local Out = Fusion.Out
+
+return function(props)
+    local absoluteContentSize = Value(Vector2.new(0, 0))
+
+    return New "Frame" {
+        BackgroundTransparency = 1,
+        Size = Computed(function()
+            return UDim2.new(1, 0, 0, absoluteContentSize.Y)
+        end),
+        Visible = Computed(function()
+            return #props.Players > 0
+        end),
+
+        [Children] = {
+            New "UIListLayout" {
+                SortOrder = Enum.SortOrder.LayoutOrder,
+                [Out "AbsoluteContentSize"] = absoluteContentSize,
+            },
+            TeamHeaderComponent({
+                Color = props.Color,
+                Name = props.Name,
+                Collapsed = props.Collapsed,
+                Count = #props.Players,
+            }),
+            ForPairs(props.Players, function(index, player)
+                return player.Name, PlayerComponent({
+                    Name = player.Name,
+                    DisplayName = player.DisplayName,
+                    Icon = Computed(function()
+                        local success, result = pcall(player.IsFriendsWith, player, Player)
+                        if success and result then
+                            return "rbxasset://textures/ui/PlayerList/FriendIcon@3x.png"
+                        elseif player.MembershipType == Enum.MembershipType.Premium then
+                            return "rbxasset://textures/ui/PlayerList/PremiumIcon@3x.png"
+                        else
+                            return "rbxasset://"
+                        end
+                    end),
+                    Order = Value(index),
+                    AtTop = Value(true),
+                    AtBottom = Computed(function()
+                        return index == #props.Players
+                    end),
+                    Visible = Computed(function()
+                        return not props.Collapsed:get()
+                    end),
+                })
+            end)
+        },
+    }
+end
--- src/Main.client.lua
+++ src/Main.client.lua
@@ -5,8 +5,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ContextActionService = game:GetService("ContextActionService")
 
 local Fusion = require(ReplicatedStorage.Packages.Fusion)
-local PlayerComponent = require(script.Parent.Components.Player)
-local TeamHeaderComponent = require(script.Parent.Components.TeamHeader)
+local List = require(script.Parent.Components.TeamList)
 local Player = Players.LocalPlayer
 
 local New = Fusion.New
@@ -17,10 +16,11 @@ local Computed = Fusion.Computed
 local Tween = Fusion.Tween
 local Out = Fusion.Out
 
-local UISize = Value(UDim2.fromOffset(200, 300))
-local UIVisible = Value(true)
-local UICanvasSize = Value(Vector2.new(0, 0))
-local teamsData = Value({})
+local size = Value(UDim2.fromOffset(200, 300))
+local isVisible = Value(true)
+local canvasSize = Value(Vector2.new(0, 0))
+local allTeams = Value({})
+
 local UI = New "ScreenGui" {
 	ZIndexBehavior = Enum.ZIndexBehavior.Sibling,
 	Parent = Player:WaitForChild("PlayerGui"),
@@ -30,77 +30,25 @@ local UI = New "ScreenGui" {
 		New "ScrollingFrame" {
 			ScrollBarThickness = 0,
 			AnchorPoint = Tween(Computed(function()
-				return UIVisible:get() and Vector2.new(1, 0) or Vector2.new(0, 0)
+				return isVisible:get() and Vector2.new(1, 0) or Vector2.new(0, 0)
 			end), TweenInfo.new(0.3, Enum.EasingStyle.Quint)),
 			BackgroundTransparency = 1,
 			CanvasSize = Tween(Computed(function()
-				return UDim2.fromOffset(0, UICanvasSize:get().Y)
+				return UDim2.fromOffset(0, canvasSize:get().Y)
 			end), TweenInfo.new(0.3, Enum.EasingStyle.Quint)),
 			Position = UDim2.new(1, -5, 0, 4),
-			Size = UISize,
+			Size = size,
 
 			[Children] = {
 				New "UIListLayout" {
 					Padding = UDim.new(0, 6),
 					SortOrder = Enum.SortOrder.LayoutOrder,
-					[Out "AbsoluteContentSize"] = UICanvasSize,
+					[Out "AbsoluteContentSize"] = canvasSize,
 				},
 
-				ForValues(teamsData, function(value)
-					local absoluteContentSize = Value(Vector2.new(0, 0))
-
-					return New "Frame" {
-						BackgroundTransparency = 1,
-						Size = Computed(function()
-							return UDim2.new(1, 0, 0, absoluteContentSize:get().Y)
-						end),
-						Visible = Computed(function()
-							return #value.Players:get() > 0
-						end),
-
-						[Children] = {
-							New "UIListLayout" {
-								SortOrder = Enum.SortOrder.LayoutOrder,
-								[Out "AbsoluteContentSize"] = absoluteContentSize,
-							},
-							TeamHeaderComponent({
-								Color = value.Color,
-								Name = value.Name,
-								Collapsed = value.Collapsed,
-								Count = Computed(function()
-									return #value.Players:get() or "0"
-								end)
-							}),
-							ForValues(value.Players, function(player)
-								local index = table.find(value.Players:get(), player)
-								return PlayerComponent({
-									Name = player.Name,
-									DisplayName = player.DisplayName,
-									Icon = Computed(function()
-										local success, result = pcall(player.IsFriendsWith, player, Player)
-										if success and result then
-											return "rbxasset://textures/ui/PlayerList/FriendIcon@3x.png"
-										elseif player.MembershipType == Enum.MembershipType.Premium then
-											return "rbxasset://textures/ui/PlayerList/PremiumIcon@3x.png"
-										else
-											return "rbxasset://"
-										end
-									end),
-									Order = Computed(function()
-										return index
-									end),
-									AtTop = Value(true),
-									AtBottom = Computed(function()
-										return index == #value.Players:get()
-									end),
-									Visible = Computed(function()
-										return not value.Collapsed:get()
-									end),
-								})
-							end),
-						},
-					}
-				end)
+				ForValues(allTeams, function(team)
+					return List(team)
+				end),
 			}
 		}
 	},
@@ -109,24 +57,16 @@ local UI = New "ScreenGui" {
 local function registerTeam(team: Team)
 	if not team:IsA("Team") then return false end
 	local function update()
-		local currentTeamsData = teamsData:get()
-		currentTeamsData[team.Name] = currentTeamsData[team.Name] or {
-			Players = Value({}),
+		local allTeamsTbl = allTeams:get()
+		local currentTeam = allTeamsTbl[team.Name]
+		local newData =  {
+			Players = team:GetPlayers(),
 			Name = team.Name,
 			Color = team.TeamColor.Color,
-			Collapsed = Value(false),
-			CollapsedDueToNoPlayers = Value(true)
+			Collapsed = if currentTeam then currentTeam.Collapsed else Value(false)
 		}
-		
-		currentTeamsData[team.Name].Players:set(team:GetPlayers())
-		if #currentTeamsData[team.Name].Players:get() == 0 then
-			currentTeamsData[team.Name].Collapsed:set(true)
-			currentTeamsData[team.Name].CollapsedDueToNoPlayers:set(true)
-		elseif currentTeamsData[team.Name].CollapsedDueToNoPlayers:get() then
-			currentTeamsData[team.Name].Collapsed:set(false)
-			currentTeamsData[team.Name].CollapsedDueToNoPlayers:set(false)
-		end
-		teamsData:set(currentTeamsData)
+		allTeamsTbl[team.Name] = newData
+		allTeams:set(allTeamsTbl, true)
 	end
 
 	team.PlayerAdded:Connect(update)
@@ -137,22 +77,23 @@ end
 Teams.ChildAdded:Connect(registerTeam)
 Teams.ChildRemoved:Connect(function(team)
 	if not team:IsA("Team") then return end
-	local newTeamsData = teamsData:get()
+	local newTeamsData = allTeams:get()
 	newTeamsData[team.Name] = nil
-	teamsData:Set(newTeamsData)
+	allTeams:set(newTeamsData, true)
 end)
 ContextActionService:BindActionAtPriority("TogglePlayerList", function(_, state)
 	if state == Enum.UserInputState.Begin then
-		UIVisible:set(not UIVisible:get())
+		isVisible:set(not isVisible:get())
 	end
 end, false, 4000, Enum.KeyCode.Tab)
 
 for _, team in ipairs(Teams:GetTeams()) do
 	registerTeam(team)
 end
-StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
 if UI.AbsoluteSize.Y > 500 then
-	UISize:set(UDim2.fromOffset(200, 300))
+	size:set(UDim2.fromOffset(200, 300))
 else
-	UISize:set(UDim2.new(0, 180, 0.4, 0))
+	size:set(UDim2.new(0, 180, 0.4, 0))
 end
+
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
